f8f5e26b07cd37063770db59053b18757f7964c4
unknown <uwaiseibna>
2021-05-05 16:54:52 +0600

    first

---
+++ ts.c
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+main()
+
+{
+    double n,m,a;
+    long long p,q;
+    p= ceil(n/a);
+    q= ceil(m/a);
+    cout<< p*q;
+    return 0;
+}

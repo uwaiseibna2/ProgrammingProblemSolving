7fe5300f8144d3eec9c93e7d059bc37c2f242271
unknown <uwaiseibna>
2021-07-23 20:44:25 +0600

    change

--- bst.cpp
+++ bst.cpp
@@ -89,6 +89,45 @@ int findHeight(bst *root)
         return max(lheight,rheight)+1;
     }
 }
+void printLevelOrder(bst *root)
+{
+    if (root ==NULL) return;
+    queue <bst *> Q;
+    Q.push(root);
+    while(!Q.empty())
+    {
+        bst *temp = Q.front();
+        cout<<temp->data<<" ";
+        Q.pop();
+        if (temp->left!= NULL) Q.push(temp->left);
+        if (temp->right!=NULL) Q.push(temp->right);
+    }
+}
+void printInorder(bst *root)
+{
+    if(root == NULL)
+    return;
+    printInorder(root->left);
+    cout<<root->data<<" ";
+    printInorder(root->right);
+}
+void printPreorder(bst *root)
+{
+    if(root == NULL)
+    return;
+    cout<<root->data<<" ";
+    printPreorder(root->left);
+    printPreorder(root->right);
+}
+void printPostorder(bst *root)
+{
+    if(root == NULL)
+    return;
+    printPostorder(root->left);
+    printPostorder(root->right);
+    cout<<root->data<<" ";
+}
+
 main()
 {
     root = NULL;
@@ -104,5 +143,12 @@ main()
     
     cout<<minbst()<<endl;
     cout<<maxbst()<<endl;
-    cout<<findHeight(root);
+    cout<<findHeight(root)<<endl;
+    printInorder(root);
+    cout<<endl;
+    printPreorder(root);
+    cout<<endl;
+    printPostorder(root);
+    cout<<endl;
+    printLevelOrder(root);
 }
